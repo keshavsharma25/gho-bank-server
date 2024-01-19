466688d6cb8797196013001182f0c89f2eaa149d
Keshav Sharma <skeshav0825@gmail.com>
2024-01-20 00:38:33 +0530

    refactor: initialize walletClient with public action

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,7 +1,17 @@
-import { createPublicClient, http } from "viem";
+import { Address, createWalletClient, http, publicActions } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 
-export const client = createPublicClient({
+export const client = createWalletClient({
   chain: sepolia,
   transport: http(),
-});
+}).extend(publicActions);
+
+export const getAccount = () => {
+  const privateKey = process.env["PRIVATE_KEY"];
+  if (!privateKey) {
+    throw new Error("Private Key does not exist in environment variables");
+  }
+
+  return privateKeyToAccount(privateKey as Address);
+};
