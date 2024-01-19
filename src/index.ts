7b09b80678a8c5ecb451f0454890d9edffbaa3c2
Keshav Sharma <skeshav0825@gmail.com>
2024-01-20 00:39:32 +0530

    feat: create `/permit` post request

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,42 @@
-import express from "express";
-import { watchAllTokenApprovals } from "./utils/watchEvents";
 import "dotenv/config";
+import express, { Request, Response } from "express";
+import { watchAllTokenApprovals } from "./utils/watchEvents";
+import bodyParser from "body-parser";
+import { dbClient } from "./utils/db";
 
 const main = async () => {
   const server = express();
 
+  server.use(bodyParser.json());
+
   console.log("Starting server...");
-  server.listen(8080);
+
+  server.post("/permit", (req: Request, res: Response) => {
+    const { account, amount, deadline, v, r, s } = req.body;
+
+    dbClient.from("BookKeep").insert({
+      account,
+      amount,
+      deadline,
+      v,
+      r,
+      s,
+      isPermit: true,
+    });
+
+    res.send("Successfuly sent value");
+  });
+
+  server.get("/", (req, res) => {
+    res.send("hello world");
+  });
 
   watchAllTokenApprovals();
+  server.listen(8080, () => {
+    console.log("listening whereever you are");
+  });
 };
 
-main()
-  .then(() => {})
-  .catch((err) => {
-    console.error(err);
-  });
+main().catch((err) => {
+  console.error(err);
+});
